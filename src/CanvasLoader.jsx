0d389238853c582aaba46468c8cca74457f81ba2
pierre <pierre@dahive.fr>
2025-12-17 15:01:30 +0100

    Preload l'image de fond

--- src/CanvasLoader.jsx
+++ src/CanvasLoader.jsx
@@ -2,8 +2,11 @@ export default function CanvasLoader() {
   return (
     <div className="factory3d-canvas-loader-container">
       <img
+        fetchPriority="high"
         className="factory3d-canvas-loader"
         src="/loader-background.webp"
+        width="1920"
+        height="1080"
         alt="Factory background"
       />
     </div>
